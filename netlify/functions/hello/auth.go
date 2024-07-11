5ae52620c068e868c695a831ac69070fe7bb74e5
Tyuzu <37517816+Tyuzu@users.noreply.github.com>
2024-07-11 11:30:41 +0530

    Add files via upload

--- netlify/functions/hello/auth.go
+++ netlify/functions/hello/auth.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"passport/internal/cookies"
+	"github.com/Tyuzu/pog/internal/cookies"
 )
 
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
