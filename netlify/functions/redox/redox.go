d24b86c9b8426e0eedd85e7ef7edc93a92030630
Tyuzu <37517816+Tyuzu@users.noreply.github.com>
2024-07-14 17:56:55 +0530

    Update redox.go

--- netlify/functions/redox/redox.go
+++ netlify/functions/redox/redox.go
@@ -3,13 +3,13 @@ package main
 import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/Tyuzu/pog/internal/rdx"
+	Rdx "github.com/Tyuzu/pog/internal/rdx"
 )
 
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-	rdx.InitRedis()
-	rdx.RdxSet("hola","amigos")
-	hola := rdx.RdxGet("hola")
+	Rdx.InitRedis()
+	Rdx.RdxSet("hola","amigos")
+	hola := Rdx.RdxGet("hola")
   return &events.APIGatewayProxyResponse{
     StatusCode:        200,
     Body:              hola,
